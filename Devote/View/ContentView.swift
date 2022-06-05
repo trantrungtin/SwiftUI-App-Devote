c926ca596a486c1504a47aa22cc16810b5811b3e
Tin Tran <tinhardware@gmail.com>
2022-06-05 11:59:25 +0700

    Update list style

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -103,7 +103,14 @@ struct ContentView: View {
                         }
                             .onDelete(perform: deleteItems)
                     } //: LIST
+                    .listStyle(InsetGroupedListStyle())
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
+                    .padding(.vertical, 0)
+                    .frame(maxWidth: 640)
                 } //: VSTACK
+                .onAppear() {
+                    UITableView.appearance().backgroundColor = UIColor.clear
+                }
             } //: ZSTACK
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {

b4098d5282c2f0d58144cbe484b92557bfff60fb
Tin Tran <tinhardware@gmail.com>
2022-06-05 10:46:25 +0700

    Update data model

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Tin Tran on 05/06/2022.
+//
+
+import Foundation
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
